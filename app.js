5718e0fce71755be363acae9d253becd8a2ce167
alxsandre <alexandr.foret@gmail.com>
2021-07-24 17:34:30 +0200

    post sign up started

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-
-const stuffRoutes = require('./routes/stuff');
-const userRoutes = require('./routes/user');
-
-mongoose.connect('mongodb+srv://alexandre:Sandre12@cluster0.jr8rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-{ useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.use(bodyParser.json());
-
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.use('/api/stuff', stuffRoutes);
-app.use('/api/auth', userRoutes);
-
+app.post('/api/auth/signup', (req, res) => {
+    console.log(req.headers)
+    console.log(req.body.email)
+    res.status(201).send('hello post')
+})
 module.exports = app;

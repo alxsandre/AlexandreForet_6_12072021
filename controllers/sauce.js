ac82f1832b44920be73ddf74bbc8714f3cd930c4
alxsandre <alexandr.foret@gmail.com>
2021-08-16 11:32:42 +0200

    modifyLike created

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -2,40 +2,50 @@ status  = require('http-status');
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
-exports.createSauce = (req, res, next) => {
+exports.createSauce = async (req, res, next) => {
+  try { 
     const sauceObject = JSON.parse(req.body.sauce);
-    //delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
         likes: 0,
         dislikes: 0,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    sauce.save()
-        .then(() => res.status(status.CREATED).json({ message: 'objet enregistré !'}))
-        .catch(error => res.status(status.BAD_REQUEST).json({ error }));
+    await sauce.save();
+    return res.status(status.CREATED).json({ message: 'objet enregistré !'})
+  } catch (error) {
+    return res.status(status.BAD_REQUEST).json({ error })
+  }
 };
 
 exports.modifyLike = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
       .then(sauce => {
       let likes = sauce.likes;
-      let usersLiked = sauce.usersLiked;
+      let dislikes = sauce.dislikes;
       let userId = req.body.userId;
-    console.log([userId]);
-
-        if (req.body.like && req.body.like === 1) {
-          likes = likes + req.body.like;
-          usersLiked = [...usersLiked, userId]
-        } else if (req.body.like && req.body.like === 0 || req.body.like === -1) {
-          likes = likes - 1;
-          if (usersLiked.includes(userId)) { usersLiked.filter(usersId => usersId !== userId) }
+      let usersLiked = sauce.usersLiked;
+      let usersDisliked = sauce.usersDisliked;
+      let userAlreadyLiked = usersLiked.includes(userId);
+      let userAlreadyDisliked = usersDisliked.includes(userId);
+        if (req.body.like === 1 && !userAlreadyLiked) {
+          likes = likes + 1;
+          usersLiked = [...usersLiked, userId];
+        } else if (!req.body.like) {
+          if (userAlreadyLiked) {
+            likes = likes - 1;
+            usersLiked = usersLiked.filter(usersId => usersId !== userId);
+          }
+          if (userAlreadyDisliked) {
+            dislikes = dislikes - 1;
+            usersDisliked = usersDisliked.filter(usersId => usersId !== userId);
+          }
+        } else if (req.body.like === -1 && !userAlreadyDisliked) {
+            dislikes = dislikes + 1;
+            usersDisliked = [...usersDisliked, userId];
         };
-
-        Sauce.updateOne({ _id: req.body.id }, { likes, usersLiked, _id: req.params.id})
-          .then((sauce) => { console.log(sauce)
-            res.status(status.OK).json({ message: 'Objet modifié!'})
-        })
+        Sauce.updateOne({ _id: req.params.id }, { likes, dislikes,  usersLiked, usersDisliked, _id: req.params.id})
+          .then(() => res.status(status.OK).json({ message: 'Objet modifié!'}))
           .catch(error => res.status(status.NOT_FOUND).json({ error }));
     })   
 };
@@ -63,15 +73,35 @@ exports.deleteSauce = (req, res, next) => {
         })
         .catch(error => res.status(status.INTERNAL_SERVER_ERROR).json({ error }))
 };
-
+/*
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
       .then(sauce => res.status(status.OK).json(sauce))
       .catch(error => res.status(status.NOT_FOUND).json({ error }))
   };
-
+*/
+  /*
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
       .then(Sauces => res.status(status.OK).json(Sauces))
       .catch(error => res.status(status.BAD_REQUEST).json({ error }))
   };
+  */
+
+exports.getOneSauce = async (req, res, next) => {
+  try {
+    const sauce = await Sauce.findOne({ _id: req.params.id })
+    return res.status(status.OK).json(sauce);
+  } catch (error) {
+    return res.status(status.NOT_FOUND).json({ error })
+  }
+};
+
+exports.getAllSauces = async (req, res, next) => {
+  try { 
+    const sauces = await Sauce.find();
+    return res.status(status.OK).json(sauces);
+  } catch (error) {
+    return res.status(status.BAD_REQUEST).json({ error });
+  }
+};

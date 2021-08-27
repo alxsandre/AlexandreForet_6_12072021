a1402156654de7ca058355a268f6421c5bdd9e62
alxsandre <alexandr.foret@gmail.com>
2021-08-27 13:11:08 +0200

    ratelimiter and ds_store ignore

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const path = require('path');
 const helmet = require('helmet');
 app.use(helmet());
 
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
 require('dotenv').config();
 
 const userRoutes = require('./routes/user');
